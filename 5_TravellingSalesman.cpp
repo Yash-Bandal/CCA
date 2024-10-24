798fffe4899298bf320105ac4e31928091b618fc
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-10-24 23:13:56 +0530

    Update 5_TravellingSalesman.cpp

--- 5_TravellingSalesman.cpp
+++ 5_TravellingSalesman.cpp
@@ -1,3 +1,74 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int n = 4;
+int dp[16][4];
+int dist[10][10] = {
+    {0, 20, 42, 25},
+    {20, 0, 30, 34},
+    {42, 30, 0, 10},
+    {25, 34, 10, 0}
+    };
+int VisitedAll = (1 << n) - 1; 
+/*
+ visited all is 1111 for 4 cities..given by binary
+ For n = 4, 1 << 4 equals 16 in binary (10000).
+ and 10000 - 1 = 1111
+
+ Subtracting 1 gives 1111 (binary), which equals 15 in decimal.
+ This 1111 means that all 4 cities have been visited (all bits are set to 1).
+
+ mask =rep visited ..eg  0110 means cities 1 and 2 have been visited (since bits 1 and 2 are set to 1).
+ pos = current city
+*/
+int tsp(int mask, int pos)
+{
+    if (mask == VisitedAll)   //ie 1111 == 1111..true
+    { 
+        //        curr city -> 0
+        return dist[pos][0];  //return the distance from the current city back to the starting city (dist[pos][0]).
+    }
+    if (dp[mask][pos] != -1)
+    {        //min dist
+        return dp[mask][pos];  //f we already calculated the minimum cost for this state (dp[mask][pos] != -1), return it
+    }
+    int ans = INT_MAX; // Using predefined INT_MAX
+    for (int city = 0; city < n; city++)
+    {
+        int cityBit = (1 << city);
+        if ((mask & cityBit) == 0)  //that is atleast 1 is 0..unvisited..And table 1 & 1 =1 else all 0
+        {
+            int newAns = dist[pos][city] + tsp(mask | cityBit, city);
+            ans = min(ans, newAns);
+        }
+    }
+    return dp[mask][pos] = ans;
+}
+int main()
+{
+    for (int i = 0; i < (1 << n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+    cout << "Minimum weight is: " << tsp(1, 0) << endl;
+    return 0;
+}
+
+/*
+tsp(1, 0): Start the TSP algorithm from city 0, with only city 0 visited.
+ The initial mask is 1 (binary 0001), which means only city 0 is visited.
+*/
+//===============================================================================
+
+
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
