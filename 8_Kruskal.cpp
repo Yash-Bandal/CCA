2062832931c68c902a143174eacc1244751cf1e3
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2024-11-05 22:10:19 +0530

    Update 8_Kruskal.cpp

--- 8_Kruskal.cpp
+++ 8_Kruskal.cpp
@@ -1,61 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// DisjointSet class to perform union-findPar with union by size
 class DisjointSet {
-    vector<int> rank, parent, size;
+    vector<int> parent, size;
+
 public:
     DisjointSet(int n) {
-        rank.resize(n + 1, 0);
         parent.resize(n + 1);
         size.resize(n + 1);
         for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+            parent[i] = i;           //[1 2 3 4 5]
+            size[i] = 1;             //[1 1 1 1 1]
         }
     }
 
-    int findUPar(int node) {
-        if (node == parent[node]) return node;
-        return parent[node] = findUPar(parent[node]);
+    // findPar with path compression
+    int findPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findPar(parent[node]);
     }
 
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        } else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        } else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
+    // Union by size
     void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+        int rootU = findPar(u);
+        int rootV = findPar(v);
+         //base
+        if (rootU == rootV) return;
+
+        if (size[rootU] < size[rootV]) {
+            parent[rootU] = rootV;
+            size[rootV] += size[rootU];
         } else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+            parent[rootV] = rootU;
+            size[rootU] += size[rootV];
         }
     }
 };
 
-class Solution {
+
+class Solution
+{
 public:
-    int spanningTree(int V, vector<vector<int>> adj[]) {
+    //Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // 1 - 2 wt = 5
+        /// 1 - > (2, 5)
+        // 2 -> (1, 5)
+
+        // 5, 1, 2
+        // 5, 2, 1
         vector<pair<int, pair<int, int>>> edges;
         for (int i = 0; i < V; i++) {
             for (auto it : adj[i]) {
                 int adjNode = it[0];
                 int wt = it[1];
                 int node = i;
+                //{node adjnode wt}
+
                 edges.push_back({wt, {node, adjNode}});
             }
         }
@@ -66,30 +70,31 @@ public:
             int wt = it.first;
             int u = it.second.first;
             int v = it.second.second;
-            if (ds.findUPar(u) != ds.findUPar(v)) {
+
+            if (ds.findPar(u) != ds.findPar(v)) {
                 mstWt += wt;
                 ds.unionBySize(u, v);
             }
         }
+
         return mstWt;
     }
 };
 
 int main() {
     int V = 5;
-    vector<vector<int>> edges = {{0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}};
-    vector<vector<int>> adj[V];
-    for (auto it : edges) {
-        vector<int> tmp(2);
-        tmp[0] = it[1];
-        tmp[1] = it[2];
-        adj[it[0]].push_back(tmp);
-        tmp[0] = it[0];
-        tmp[1] = it[2];
-        adj[it[1]].push_back(tmp);
-    }
+
+    // Directly defining adj with edges
+    vector<vector<int>> adj[V] = {
+        {{1, 2}, {2, 1}}, //adj[0]
+        {{0, 2}, {2, 1}}, // Node 1 has edges to node 0 (weight 2) and node 2 (weight 1)
+        {{0, 1}, {1, 1}, {3, 2}, {4, 2}}, // Node 2 has edges to nodes 0, 1, 3, and 4
+        {{2, 2}, {4, 1}}, // Node 3 has edges to node 2 (weight 2) and node 4 (weight 1)
+        {{3, 1}, {2, 2}}  // Node 4 has edges to node 3 (weight 1) and node 2 (weight 2)
+    };
+
     Solution obj;
-    int mstWt = obj.spanningTree(V, adj);
-    cout << "The sum of all the edge weights: " << mstWt << endl;
+    int mstWeight = obj.spanningTree(V, adj);
+    cout << "The sum of all the edge weights: " << mstWeight << endl;
     return 0;
 }
